8fa70cd8a474deb6a76bc60b18b8f4c8f6bab778
BhushanPatil2598 <139616811+BhushanPatil2598@users.noreply.github.com>
2023-07-15 14:39:20 +0530

    Add files via upload

---
+++ PythonAssignment2(1).py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Write a Python program that accepts a word from the user and reverse it.
+# 
+# 
+# 
+# Sample Test Case
+# 
+# 
+# 
+# Input : Edyoda
+# 
+# output: adoydE
+
+# In[2]:
+
+
+word = input("Enter The Word: ")
+reversed = word[::-1]
+print("This Is Reversed Word: ",reversed)
+
+
+# In[ ]:
+
+
+
+
