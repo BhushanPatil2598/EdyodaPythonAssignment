fba8d1c8d721d07c65cf85d737ecb776ee988af1
BhushanPatil2598 <139616811+BhushanPatil2598@users.noreply.github.com>
2023-07-28 19:18:20 +0530

    Add files via upload

---
+++ Assignment2(Q1)Fun with list and tuple.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+Write a Python program to get a list, sorted in increasing order by the last element in each tuple from a given list of non-empty tuples
+
+
+Sample List : [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+
+Expected Result : [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]
+# In[1]:
+
+
+### With no user input values
+
+def last(n): return n[-1]
+
+def sort_list_last(tuples):
+  return sorted(tuples, key=last)
+
+print(sort_list_last([(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]))
+
+
+# In[ ]:
+
+
+### With user input values
+
+def last(n): return n[-1]
+
+def sort_list_last(tuples):
+  return sorted(tuples, key=last)
+a=input("Enter a list of tuples:")
+print(sort_list_last(a))
+
+
+# In[ ]:
+
+
+
+
