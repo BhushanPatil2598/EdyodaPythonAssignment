cb73a8c1c504bbc2b4657c9fe638dc6f294ec4e2
BhushanPatil2598 <139616811+BhushanPatil2598@users.noreply.github.com>
2023-09-09 06:22:03 +0530

    Add files via upload

---
+++ Assignment2_LinearData.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Q1.Delete the elements in an linked list whose sum is equal to zero
+
+# In[ ]:
+
+
+
+
+
+# Q2.Reverse a linked list in groups of given size
+
+# In[ ]:
+
+
+
+
+
+# Q3.Merge a linked list into another linked list at alternate positions.
+
+# In[ ]:
+
+
+
+
+
+# Q4.In an array, Count Pairs with given sum
+
+# In[ ]:
+
+
+
+
+
+# Q5.Find duplicates in an array
+
+# In[ ]:
+
+
+
+
+
+# Q6.Find the Kth largest and Kth smallest number in an array
+
+# In[ ]:
+
+
+
+
